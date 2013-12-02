0c76fbc7bdfafccde20e991cc499bdd982db3777
claudiu <claudiu.garba@gmail.com>
2013-12-02 22:55:17 +0100

    Initial commit

---
+++ js/controllers.js
@@ -0,0 +1,13 @@
+/**
+ * Created by claudiu on 12/2/13.
+ */
+
+var phoneApp = angular.module('phoneApp', []);
+
+phoneApp.controller('phoneList', function($scope){
+    $scope.phones = [
+        {'name': 'moto g', 'snippet': 'my phone'},
+        {'name': 'iphone', 'snippet': 'girlfriend phone'},
+        {'name': 'google x', 'snippet': 'next phone'}
+    ]
+})
